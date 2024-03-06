7fa2b70055482bd42d1cd3453d4eaf5f1d492849
Dima Berry <dimaberry1@gmail.com>
2024-03-06 19:30:58 +0100

    solve component2

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <AppTitle />
       <Rating />
       <Accordion />
+      <Rating />
     </div>
   );
+}
+
+function AppTitle() {
+  return <>Hell</>;
 }
 
 function Rating() {
   return (
     <>
+      <Star />
       <Star />
       <Star />
       <Star />
@@ -27,13 +34,23 @@ function Star() {
 function Accordion() {
   return (
     <div>
-      <h3>Menu</h3>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+      <AccordionTitle />
+      <AccordionBody />
     </div>
+  );
+}
+
+function AccordionTitle() {
+  return <h3>Menu</h3>;
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
   );
 }
 
